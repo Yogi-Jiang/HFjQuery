d83d73f5c33bd40d224a6e6f3b72e56f09926ae0
Yogi.Jiang <Yogi.Jiang@hotmail.com>
2015-06-22 07:32:16 +0800

    15.6.22.01

--- ch08/scripts/my_script.js
+++ ch08/scripts/my_script.js
@@ -30,29 +30,80 @@ $(document).ready(function() {
                     };
                     $("#finishers_all").append(info);
                 });
-                getTime();
+                getTimeAjax();
             }
         });
     }
 
-    function getTime() {
-        var a_p = "";
-        var d = new Date();
-        var curr_hour = d.getHours();
-        (curr_hour < 12) ? a_p = "AM": a_p = "PM";
-        (curr_hour == 0) ? curr_hour = 12: curr_hour = curr_hour;
-        (curr_hour > 12) ? curr_hour = curr_hour - 12: curr_hour = curr_hour;
-        var curr_min = d.getMinutes().toString();
-        var curr_sec = d.getSeconds().toString();
-        if (curr_min.length == 1) {
-            curr_min = "0" + curr_min;
-        };
-        if (curr_sec.length == 1) {
-            curr_sec = "0" + curr_sec;
-        };
-        $("#updatedTime").html(curr_hour + ":" + curr_min + ":" + curr_sec + " " + a_p);
+    function showFrequency() {
+        $("#freq").html("Page refreshes every " + FREQ/1000 + " seconds.");
+    }
+
+    showFrequency();
+
+    function getTimeAjax() {
+        console.log("1");
+        $('#updatedTime').load("time.php",function(response, status, xhr) {
+            console.log("status:" + status);
+            console.log("response:" + response);
+        });
+        console.log("3");
     }
 
     getXMLRacers();
     startAjaxcalls();
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ ch08/time.php
@@ -0,0 +1,5 @@
+<?php
+    date_default_timezone_set('Asia/Shanghai');
+    echo date("F j, Y, g:i:s a");
+
+?>
