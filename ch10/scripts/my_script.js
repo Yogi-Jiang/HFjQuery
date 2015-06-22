33934128311e547ea8ec4c48378dd9226419c959
Yogi.Jiang <Yogi.Jiang@hotmail.com>
2015-06-23 06:30:20 +0800

    15.6.23.01

---
+++ ch10/scripts/my_script.js
@@ -0,0 +1,10 @@
+/**
+ * Created by wanjie on 2015/6/23.
+ */
+$(document).ready(function() {
+    $("#datepicker").datepicker({
+        changeMonth: true,
+        changeYear: true
+    });
+    $("#type_select").buttonset();
+})
